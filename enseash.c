ece28b652c0e6256a5ecd80622fd9e6a997073a8
HAASNicolas <nicolas.haas@netc.fr>
2024-12-03 08:53:11 +0100

    Q.1 bug fix

--- enseash.c
+++ enseash.c
@@ -7,8 +7,8 @@
 
 int main(int argc, char *argv[]) {
 	
-	write(argv[1], "$ ./enseash\n", 12);
-	write(argv[1], "Bienvenue dans le Shell ENSEA.\n", 31);
-	write(argv[1], "Pour quitter, tapez 'exit'.\n", 28);
-	write(argv[1], "enseash % ", 10);
+	write(STDOUT_FILENO, "$ ./enseash\n", 12);
+	write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\n", 31);
+	write(STDOUT_FILENO, "Pour quitter, tapez 'exit'.\n", 28);
+	write(STDOUT_FILENO, "enseash % ", 10);
 }
