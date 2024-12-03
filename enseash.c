42af111c1fd6e0a01c278cc8259b300da8e9b9e2
HAASNicolas <nicolas.haas@netc.fr>
2024-12-03 09:13:08 +0100

    Q.1 add Failure

--- enseash.c
+++ enseash.c
@@ -4,12 +4,19 @@
 #include <unistd.h>
 
 int main() {
-// Using STDOUT_FILENO to write to standard output (the console)
-    write(STDOUT_FILENO, "$ ./enseash\n", 12);
-    write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\n", 31);
-    write(STDOUT_FILENO, "Pour quitter, tapez 'exit'.\n", 28);
-    write(STDOUT_FILENO, "enseash % ", 10);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-    return 0; // Returns 0 to indicate that everything went well
+int main(int argc, char *argv[]) {
+	// Using STDOUT_FILENO to write to standard output (the console)
+    
+	if (write(STDOUT_FILENO, "$ ./enseash\n", 12) == -1) {exit(EXIT_FAILURE);};
+	if (write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\n", 31) == -1) {exit(EXIT_FAILURE);};
+	if (write(STDOUT_FILENO, "Pour quitter, tapez 'exit'.\n", 28) == -1) {exit(EXIT_FAILURE);};
+	if (write(STDOUT_FILENO, "enseash % ", 10) == -1) {exit(EXIT_FAILURE);};
+	
+	exit(EXIT_SUCCESS); // Returns SUCCESS to indicate that everything went well
 }
 
