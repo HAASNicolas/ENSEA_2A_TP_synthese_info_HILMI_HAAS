83875f58e9d8db68631e66ca2589bc272c50f864
nanas03 <anas.hilmi02@gmail.com>
2024-12-03 08:55:10 +0100

    Update enseash.c

--- enseash.c
+++ enseash.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-	
-	write(STDOUT_FILENO, "$ ./enseash\n", 12);
-	write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\n", 31);
-	write(STDOUT_FILENO, "Pour quitter, tapez 'exit'.\n", 28);
-	write(STDOUT_FILENO, "enseash % ", 10);
+int main() {
+// Using STDOUT_FILENO to write to standard output (the console)
+    write(STDOUT_FILENO, "$ ./enseash\n", 12);
+    write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\n", 31);
+    write(STDOUT_FILENO, "Pour quitter, tapez 'exit'.\n", 28);
+    write(STDOUT_FILENO, "enseash % ", 10);
+
+    return 0; // Returns 0 to indicate that everything went well
 }
+
